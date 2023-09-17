0720052789bc2a561d8070be47e4ae024d0e54a7
Tobias Weber <tobias.weber@tum.de>
2023-09-17 21:28:01 +0200

    testing tiny computer

---
+++ cpu/65xx/test_io.asm
@@ -0,0 +1,184 @@
+;
+; i/o test program
+; @author Tobias Weber
+; @date 16-sep-2023
+; @license see 'LICENSE' file
+;
+
+; -----------------------------------------------------------------------------
+; memory layout
+; -----------------------------------------------------------------------------
+ZERO_PAGE     = $0000
+STACK_PAGE    = $0100
+IO_START      = $c000
+ROM_START     = $e000
+; -----------------------------------------------------------------------------
+
+
+; -----------------------------------------------------------------------------
+; I/O registers, see: https://en.wikipedia.org/wiki/WDC_65C22
+; -----------------------------------------------------------------------------
+IO_PORT1      = $c001
+IO_PORT2      = $c000
+IO_PORT1_WR   = $c003
+IO_PORT2_WR   = $c002
+IO_AUX_CTRL   = $c00b
+IO_PORTS_CTRL = $c00c
+IO_INT_FLAGS  = $c00d
+IO_INT_ENABLE = $c00e
+; -----------------------------------------------------------------------------
+
+
+; -----------------------------------------------------------------------------
+; variable addresses
+; -----------------------------------------------------------------------------
+pattern       = $b000
+shift_dir     = $b001
+
+sleep1        = $b010
+sleep2        = $b011
+sleep3        = $b012
+sleep1_const  = $b013
+sleep2_const  = $b014
+sleep3_const  = $b015
+; -----------------------------------------------------------------------------
+
+
+
+; -----------------------------------------------------------------------------
+; entry point
+; -----------------------------------------------------------------------------
+main:
+	sei
+	clc
+	cld
+	clv
+
+	; stack pointer relative to STACK_PAGE -> 0x01ff
+	ldx #$ff
+	txs
+
+	; output to port 1
+	lda #$ff
+	sta IO_PORT1_WR
+
+	; input from port 2
+	;lda #$00
+	;sta IO_PORT2_WR
+
+	; set delay constants
+	lda #$01
+	sta sleep3_const
+	lda #$ff
+	sta sleep2_const
+	lda #$ff
+	sta sleep1_const
+
+	lda #$00
+	sta shift_dir
+	lda #$01
+	sta pattern
+
+	main_loop:
+		; latch pattern to port 1
+		lda pattern
+		sta IO_PORT1
+
+		lda shift_dir
+		;cmp #$00
+		bne shift_right
+
+		shift_left:
+			clc
+			lda pattern
+			rol
+
+			cmp #$80
+			bne cont
+
+			lda #$01
+			sta shift_dir
+			lda #$80
+
+			jmp cont
+
+		shift_right:
+			clc
+			lda pattern
+			ror
+
+			cmp #$01
+			bne cont
+
+			lda #$00
+			sta shift_dir
+			lda #$01
+
+			jmp cont
+
+		cont:
+
+		; test input from port 2
+		;lda IO_PORT2
+
+		; save new pattern
+		sta pattern
+
+		jsr sleep
+		dec sleep1_const
+
+		jmp main_loop
+
+	stp
+
+
+
+;
+; busy waiting using loops
+;
+sleep:
+	lda sleep3_const
+	sta sleep3
+	sleep_loop_3:
+		lda sleep2_const
+		sta sleep2
+		sleep_loop_2:
+			lda sleep1_const
+			sta sleep1
+			sleep_loop_1:
+				dec sleep1
+				lda sleep1
+				bne sleep_loop_1
+			dec sleep2
+			lda sleep2
+			bne sleep_loop_2
+		dec sleep3
+		lda sleep3
+		bne sleep_loop_3
+	rts
+; -----------------------------------------------------------------------------
+
+
+
+; -----------------------------------------------------------------------------
+; interrupt service routines
+; -----------------------------------------------------------------------------
+nmi_main:
+	rti
+
+
+
+isr_main:
+	rti
+; -----------------------------------------------------------------------------
+
+
+
+; -----------------------------------------------------------------------------
+; table with entry point function pointers
+; -----------------------------------------------------------------------------
+.segment "JMPTAB"
+	.addr nmi_main
+	.addr main
+	.addr isr_main
+; -----------------------------------------------------------------------------
---
+++ cpu/65xx/test_mem.asm
@@ -0,0 +1,170 @@
+;
+; memory test program
+; @author Tobias Weber
+; @date 16-sep-2023
+; @license see 'LICENSE' file
+;
+
+; -----------------------------------------------------------------------------
+; memory layout
+; -----------------------------------------------------------------------------
+ZERO_PAGE     = $0000
+STACK_PAGE    = $0100
+IO_START      = $c000
+ROM_START     = $e000
+; -----------------------------------------------------------------------------
+
+
+; -----------------------------------------------------------------------------
+; I/O registers, see: https://en.wikipedia.org/wiki/WDC_65C22
+; -----------------------------------------------------------------------------
+IO_PORT1      = $c001
+IO_PORT2      = $c000
+IO_PORT1_WR   = $c003
+IO_PORT2_WR   = $c002
+IO_AUX_CTRL   = $c00b
+IO_PORTS_CTRL = $c00c
+IO_INT_FLAGS  = $c00d
+IO_INT_ENABLE = $c00e
+; -----------------------------------------------------------------------------
+
+
+; -----------------------------------------------------------------------------
+; memory test
+; -----------------------------------------------------------------------------
+START_PAGE    = $01
+END_PAGE      = $c0
+TEST_PATTERN1 = %10101010
+TEST_PATTERN2 = %01010101
+
+; page address (in litte endian)
+page_ptr_1     = $0001
+page_ptr_2     = $0000
+; -----------------------------------------------------------------------------
+
+
+; -----------------------------------------------------------------------------
+; entry point
+; -----------------------------------------------------------------------------
+main:
+	; init
+	sei
+	clc
+	cld
+	clv
+
+	; stack pointer relative to STACK_PAGE -> 0x01ff
+	ldx #$ff
+	txs
+
+	; output status to port 1
+	lda #$ff
+	sta IO_PORT1_WR
+	lda #%01111111
+	sta IO_PORT1
+
+	; test zero page memory
+	ldy #$00
+	addr_loop_zp:
+		; write test pattern 1
+		lda TEST_PATTERN1
+		sta ZERO_PAGE, y
+
+		; read test pattern 1 back in
+		lda #$00
+		lda ZERO_PAGE, y
+		cmp TEST_PATTERN1
+		bne test_error
+
+		; write test pattern 2
+		lda TEST_PATTERN2
+		sta ZERO_PAGE, y
+
+		; read test pattern 2 back in
+		lda #$00
+		lda ZERO_PAGE, y
+		cmp TEST_PATTERN2
+		bne test_error
+
+		iny
+		cpy #$00
+		bne addr_loop_zp
+
+	; output status to port 1
+	lda #%00111111
+	sta IO_PORT1
+
+	; test the rest of the memory
+	ldx START_PAGE
+	page_loop:
+		lda #$00
+		sta page_ptr_2
+		stx page_ptr_1
+
+		ldy #$00
+		addr_loop:
+			; write test pattern 1
+			lda TEST_PATTERN1
+			sta (page_ptr_2), y
+
+			; read test pattern 1 back in
+			lda #$00
+			lda (page_ptr_2), y
+			cmp TEST_PATTERN1
+			bne test_error
+
+			; write test pattern 2
+			lda TEST_PATTERN2
+			sta (page_ptr_2), y
+
+			; read test pattern 2 back in
+			lda #$00
+			lda (page_ptr_2), y
+			cmp TEST_PATTERN2
+			bne test_error
+
+			iny
+			cpy #$00
+			bne addr_loop
+		inx
+		cpx END_PAGE
+		bne page_loop
+
+	; output status to port 1
+	lda #%00011111
+	sta IO_PORT1
+
+	jmp end_test
+	test_error:
+		; output error status to port 1
+		lda #%11111111
+		sta IO_PORT1
+
+	end_test:
+		stp
+; -----------------------------------------------------------------------------
+
+
+
+; -----------------------------------------------------------------------------
+; interrupt service routines
+; -----------------------------------------------------------------------------
+nmi_main:
+	rti
+
+
+
+isr_main:
+	rti
+; -----------------------------------------------------------------------------
+
+
+
+; -----------------------------------------------------------------------------
+; table with entry point function pointers
+; -----------------------------------------------------------------------------
+.segment "JMPTAB"
+	.addr nmi_main
+	.addr main
+	.addr isr_main
+; -----------------------------------------------------------------------------
