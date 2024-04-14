7b3c22147b2a3789dabcd516ad2af3aa37edf25c
Tobias Weber <tobias.weber@tum.de>
2024-04-14 02:35:39 +0200

    continued with font generation

--- tools/genfont/genfont.cpp
+++ tools/genfont/genfont.cpp
@@ -14,11 +14,11 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
-#include <bitset>
 
 #include <ft2build.h>
 #include <freetype/freetype.h>
 
+#include <boost/dynamic_bitset.hpp>
 #include <boost/algorithm/string.hpp>
 #include <boost/program_options.hpp>
 namespace args = boost::program_options;
@@ -39,6 +39,8 @@ struct Config
 
 	unsigned int target_height = 16;
 	unsigned int target_top = 12;
+
+	unsigned int pitch_bits = 8;
 	int target_pitch = 1;
 
 	::FT_ULong ch_first = 0x20;
@@ -61,17 +63,17 @@ bool create_font_c(::FT_Face& face, const Config& cfg)
 	}
 
 
-	(*ostr) << "#include <stdlib.h>\n\n";
+	(*ostr) << "#include <stdint.h>\n\n";
 
 	(*ostr) << "const uint16_t g_characters_first = " << cfg.ch_first << ";\n";
 	(*ostr) << "const uint16_t g_characters_last = " << cfg.ch_last << ";\n";
 	(*ostr) << "const uint16_t g_characters_pitch = " << cfg.target_pitch << ";\n";
-	(*ostr) << "const uint16_t g_characters_width = " << cfg.target_pitch*8 << ";\n";
+	(*ostr) << "const uint16_t g_characters_width = " << cfg.target_pitch * cfg.pitch_bits << ";\n";
 	(*ostr) << "const uint16_t g_characters_height = " << cfg.target_height << ";\n\n";
 
 	// see: https://www.arduino.cc/reference/en/language/variables/utilities/progmem/
 	(*ostr) << "const uint8_t g_characters[" << (cfg.ch_last - cfg.ch_first) << "]"
-		<< "[" << cfg.target_height*cfg.target_pitch << "] PROGMEM = \n{\n";
+		<< "[" << cfg.target_height*cfg.target_pitch << "] PROGMEM =\n{\n";
 
 	for(::FT_ULong ch = cfg.ch_first; ch < cfg.ch_last; ++ch)
 	{
@@ -91,16 +93,18 @@ bool create_font_c(::FT_Face& face, const Config& cfg)
 		auto output_bits = [pitch, bitmap, ostr, &cfg](
 			unsigned int y, unsigned int shift_x, bool force_zero)
 		{
-			for(int x=0; x<std::min(pitch, cfg.target_pitch); ++x)
+			for(int x = 0; x < std::min(pitch, cfg.target_pitch); ++x)
 			{
 				unsigned char byte = force_zero ? 0 : bitmap[y*pitch + x];
+				if(x == 0)
+					byte >>= shift_x;
 
 				if(x == 0)
 					(*ostr) << "\t\t";
 
-				std::bitset<8> bits(byte >> shift_x);
-				(*ostr) << "0b" << bits << ", ";
+				(*ostr) << "0b" << boost::dynamic_bitset{cfg.pitch_bits, byte} << ",";
 			}
+
 			(*ostr) << "\n";
 		};
 
@@ -113,34 +117,46 @@ bool create_font_c(::FT_Face& face, const Config& cfg)
 			<< ", bearing y: " << (metrics->horiBearingY >> 6)
 			<< ", left: " << glyph->bitmap_left
 			<< ", top: " << glyph->bitmap_top
-			<< " */"<< std::endl;
+			<< " */\n";
 
 		(*ostr) << "\t{\n";
 
 		FT_Int shift_x = glyph->bitmap_left;
-		FT_Int shift_y = cfg.target_top - glyph->bitmap_top;
+		FT_Int shift_y = cfg.target_top;
+		if(glyph->bitmap_top > 0)
+			shift_y -= glyph->bitmap_top;
 
 		// top filler
-		unsigned int top_filler_size = (cfg.target_height-(height + shift_y))/2 + shift_y;
+		unsigned int cur_y_top = 0;
+		int top_filler_size = (cfg.target_height-(height + shift_y))/2 + shift_y;
+		if(top_filler_size < 0)
+			top_filler_size = 0;
+
 		if(height + shift_y < cfg.target_height)
 		{
-			for(unsigned int y=0; y<=top_filler_size; ++y)
+			for(int y = 0; y <= top_filler_size; ++y)
+			{
 				output_bits(y, 0, true);
+				++cur_y_top;
+			}
 		}
 
 		// char
-		for(unsigned int y=0; y<std::min(height, cfg.target_height); ++y)
+		unsigned int cur_y = cur_y_top;
+		for(unsigned int y = 0; y < std::min(height, cfg.target_height - cur_y_top); ++y)
+		{
 			output_bits(y, shift_x, false);
+			++cur_y;
+		}
 
 		// bottom filler
-		for(unsigned int y=height + top_filler_size + 1; y<cfg.target_height; ++y)
+		for(unsigned int y = cur_y; y < cfg.target_height; ++y)
 			output_bits(y, 0, true);
 
 		(*ostr) << "\t},\n";
 	}
 
-	(*ostr) << "};";
-	(*ostr) << std::endl;
+	(*ostr) << "};" << std::endl;
 
 
 	// clean up
@@ -170,30 +186,142 @@ bool create_font_vhdl(::FT_Face& face, const Config& cfg)
 
 
 	(*ostr) << "library ieee;\n"
-		<< "use ieee.std_logic_1164.all\n"
+		<< "use ieee.std_logic_1164.all;\n"
 		<< "use work.conv.all;\n\n";
 
-	(*ostr) << "entity " << cfg.entity_name << " is\n"
+	(*ostr) << "\nentity " << cfg.entity_name << " is\n"
 		<< "\tgeneric(\n"
 		<< "\t\tconstant FIRST_CHAR : natural := " << cfg.ch_first << ";\n"
 		<< "\t\tconstant LAST_CHAR : natural := " << cfg.ch_last << ";\n"
 		<< "\t\tconstant CHAR_PITCH : natural := " << cfg.target_pitch << ";\n"
-		<< "\t\tconstant CHAR_WIDTH : natural := " << cfg.target_pitch*8 << ";\n"
+		<< "\t\tconstant CHAR_WIDTH : natural := " << cfg.target_pitch * cfg.pitch_bits << ";\n"
 		<< "\t\tconstant CHAR_HEIGHT : natural := " << cfg.target_height << "\n"
 		<< "\t);\n\n"
 		<< "\tport(\n"
+		<< "\t\tin_char : in std_logic_vector(" << std::ceil(std::log2(cfg.ch_last)) << " downto 0);\n"
+		<< "\t\tin_x : in std_logic_vector(" << std::ceil(std::log2(cfg.target_pitch * cfg.pitch_bits)) << " downto 0);\n"
+		<< "\t\tin_y : in std_logic_vector(" << std::ceil(std::log2(cfg.target_height)) << " downto 0);\n"
+		<< "\t\tout_pixel : out std_logic\n"
 		<< "\t);\n\n"
-		<< "end entity\n\n";
+		<< "end entity;\n\n";
 
 	(*ostr) << "\narchitecture " << cfg.entity_name << "_impl of " << cfg.entity_name << " is\n";
 
-	// TODO
+	(*ostr) << "\tsubtype t_line is std_logic_vector(CHAR_WIDTH-1 downto 0);\n"
+		<< "\ttype t_char is array(0 to CHAR_HEIGHT-1) of t_line;\n"
+		<< "\ttype t_chars is array(FIRST_CHAR to LAST_CHAR-1) of t_char;\n";
 
-	(*ostr) << "begin\n";
+	(*ostr) << "\n\tconstant chars : t_chars :=\n\t(";
 
-	// TODO
+	for(::FT_ULong ch = cfg.ch_first; ch < cfg.ch_last; ++ch)
+	{
+		if(::FT_Load_Char(face, ch, FT_LOAD_TARGET_MONO | FT_LOAD_NO_HINTING | FT_LOAD_RENDER))
+		{
+			std::cerr << "Error: Cannot load char 0x" << std::hex << ch << "." << std::endl;
+			continue;
+		}
 
-	(*ostr) << "end architecture;\n";
+		const auto *glyph = face->glyph;
+		const auto *metrics = &glyph->metrics;
+		const unsigned int height = glyph->bitmap.rows;
+		const unsigned int width = glyph->bitmap.width;
+		const int pitch = glyph->bitmap.pitch;
+		const unsigned char *bitmap = glyph->bitmap.buffer;
+
+		auto output_bits = [pitch, bitmap, ostr, &cfg](
+			unsigned int y, unsigned int shift_x, bool force_zero)
+		{
+			int max_x = std::min(pitch, cfg.target_pitch);
+
+			(*ostr) << "\t\t\t\"";
+
+			for(int x = 0; x < max_x; ++x)
+			{
+				unsigned char byte = force_zero ? 0 : bitmap[y*pitch + x];
+				if(x == 0)
+					byte >>= shift_x;
+
+				(*ostr) << boost::dynamic_bitset{cfg.pitch_bits, byte};
+			}
+
+			(*ostr) << "\"";
+		};
+
+		(*ostr) << "\n\t\t-- char number " << ch
+			<< ": '" << static_cast<char>(ch) << "'"
+			<< ", height: " << height
+			<< ", width: " << width
+			<< ", pitch: " << pitch
+			<< ", bearing x: " << (metrics->horiBearingX >> 6)
+			<< ", bearing y: " << (metrics->horiBearingY >> 6)
+			<< ", left: " << glyph->bitmap_left
+			<< ", top: " << glyph->bitmap_top
+			<< "\n";
+
+		(*ostr) << "\t\t(\n";
+
+		FT_Int shift_x = glyph->bitmap_left;
+		FT_Int shift_y = cfg.target_top;
+		if(glyph->bitmap_top > 0)
+			shift_y -= glyph->bitmap_top;
+
+		// top filler
+		int top_filler_size = (cfg.target_height-(height + shift_y))/2 + shift_y;
+		if(top_filler_size < 0)
+			top_filler_size = 0;
+
+		unsigned int cur_y_top = 0;
+		if(height + shift_y < cfg.target_height)
+		{
+			for(int y = 0; y <= top_filler_size; ++y)
+			{
+				output_bits(y, 0, true);
+				++cur_y_top;
+
+				if((unsigned int)y < cfg.target_height - 1)
+					(*ostr) << ",";
+				(*ostr) << " -- top filler line " << y + 1;
+				(*ostr) << "\n";
+			}
+		}
+
+		// char
+		unsigned int cur_y = cur_y_top;
+		for(unsigned int y = 0; y < std::min(height, cfg.target_height - cur_y_top); ++y)
+		{
+			output_bits(y, shift_x, false);
+			++cur_y;
+
+			if(y + cur_y_top < cfg.target_height - 1)
+				(*ostr) << ",";
+			(*ostr) << " -- char line " << y + 1;
+			(*ostr) << "\n";
+		}
+
+		// bottom filler
+		for(unsigned int y = cur_y; y < cfg.target_height; ++y)
+		{
+			output_bits(y, 0, true);
+
+			if(y < cfg.target_height - 1)
+				(*ostr) << ",";
+			(*ostr) << " -- bottom filler line " << y - cur_y + 1;
+			(*ostr) << "\n";
+		}
+
+		(*ostr) << "\t\t)";
+		if(ch < cfg.ch_last - 1)
+			(*ostr) << ",";
+		(*ostr) << "\n";
+	}
+
+	(*ostr) << "\n\t);\n";
+
+	(*ostr) << "\nbegin\n";
+
+	(*ostr) << "\n\tout_pixel <= chars(to_int(in_char))(to_int(in_y))(to_int(in_x));\n";
+
+	(*ostr) << "\nend architecture;" << std::endl;
 
 	return true;
 }
@@ -231,7 +359,9 @@ int main(int argc, char **argv)
 		("target_top", args::value<decltype(cfg.target_top)>(&cfg.target_top),
 			("target top, default: " + std::to_string(cfg.target_top)).c_str())
 		("target_pitch", args::value<decltype(cfg.target_pitch)>(&cfg.target_pitch),
-			("target pitch, default: " + std::to_string(cfg.target_pitch)).c_str());
+			("target pitch, default: " + std::to_string(cfg.target_pitch)).c_str())
+		("pitch_bits", args::value<decltype(cfg.pitch_bits)>(&cfg.pitch_bits),
+			("bits per pitch, default: " + std::to_string(cfg.pitch_bits)).c_str());
 
 	auto argparser = args::command_line_parser{argc, argv};
 	argparser.style(args::command_line_style::default_style);
